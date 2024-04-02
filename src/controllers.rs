85a2563a3472fb039398022791aa64e02814728e
aedius <Aedius@users.noreply.github.com>
2024-04-02 23:21:02 +0200

    finish first login step

--- src/controllers.rs
+++ src/controllers.rs
@@ -1,6 +1,6 @@
 use crate::model::MariadDb;
 use rocket::form::Form;
-use rocket::http::CookieJar;
+use rocket::http::{CookieJar, Status};
 use rocket::response::Redirect;
 use rocket::{Route, State};
 use rocket_dyn_templates::{context, Template};
@@ -19,21 +19,35 @@ struct Login<'r> {
     pseudo: &'r str,
 }
 
+const COOKIE_NAME: &str = "RSESSID";
+
 #[post("/login", data = "<login>")]
 async fn login(
     login: Form<Login<'_>>,
-    _maria_db: &State<MariadDb>,
+    maria_db: &State<MariadDb>,
     cookies: &CookieJar<'_>,
-) -> Redirect {
-    cookies.add_private(("pseudo", login.pseudo.to_string()));
+) -> Result<Redirect, Status> {
+    let user = maria_db.get_user(login.pseudo).await.map_err(|e| {
+        dbg!(e);
+        Status::InternalServerError
+    })?;
 
-    Redirect::to(uri!(account()))
+    let user = match user {
+        Some(u) => u,
+        None => maria_db.create_user(login.pseudo).await.map_err(|e| {
+            dbg!(e);
+            Status::InternalServerError
+        })?,
+    };
+
+    cookies.add_private((COOKIE_NAME, user.uuid().to_string()));
+    Ok(Redirect::to(uri!(account())))
 }
 
 #[get("/account")]
 async fn account(cookies: &CookieJar<'_>, _maria_db: &State<MariadDb>) -> Template {
     let data = cookies
-        .get_private("pseudo")
+        .get_private(COOKIE_NAME)
         .map_or("no cookie".to_string(), |crumb| crumb.value().to_string());
 
     Template::render(
--- src/model.rs
+++ src/model.rs
@@ -1,12 +1,18 @@
 use uuid::Uuid;
 
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use sqlx::{MySql, Pool};
 
 pub struct User {
-    uuid: Uuid,
+    uuid: String,
     pseudo: String,
-    role: String,
+    role: Option<String>,
+}
+
+impl User {
+    pub fn uuid(&self) -> Uuid {
+        self.uuid.parse().unwrap()
+    }
 }
 
 pub struct MariadDb {
@@ -18,18 +24,29 @@ impl MariadDb {
         MariadDb { db }
     }
 
-    pub async fn get_user(&self, pseudo: &str) -> Result<User> {
-        let users = sqlx::query!(
-            "
-SELECT uuid, pseudo, role
-FROM account
-WHERE pseudo = ?
+    pub async fn get_user(&self, pseudo: &str) -> Result<Option<User>> {
+        let user = sqlx::query_as!(
+            User,
+            "SELECT uuid, pseudo, role FROM account WHERE pseudo = ?",
+            pseudo
+        )
+        .fetch_all(&self.db)
+        .await?;
+
+        Ok(user.into_iter().next())
+    }
+
+    pub async fn create_user(&self, pseudo: &str) -> Result<User> {
+        let _ = sqlx::query!(
+            "insert into account (uuid, pseudo, role) values (uuid(), ?, null)
         ",
             pseudo
         )
-        .fetch_all(&self.db) // -> Vec<{ country: String, count: i64 }>
+        .execute(&self.db)
         .await?;
 
-        todo!("WIP !")
+        self.get_user(pseudo)
+            .await?
+            .ok_or(anyhow!("user created not found"))
     }
 }
